d098199a549e3fe555aa5fa7223465f2f9a92809
Mantra <mantratrambadia1@gmail.com>
2021-10-30 11:53:14 +0530

    update

--- exercise-counter/src/App.js
+++ exercise-counter/src/App.js
@@ -10,7 +10,17 @@ function App() {
     }))
   }
 
-  const strToint = (str)=>{
+  const ntg =()=>{
+    console.log("huh")
+  }
+
+  let audio = new Audio('https://www.soundjay.com/buttons/sounds/beep-07a.mp3');
+
+    const playsound = ()=>{
+        audio.play();
+    }
+
+    const strToint = (str)=>{
       let n=str.length
       let ans=0
       let temp=1
@@ -22,6 +32,24 @@ function App() {
       return ans
     }
 
+  let t=0
+
+  const onStart = ()=>{
+    let n=exercises.length
+    for(let i=0;i<n;i++)
+    {
+      let x=exercises[i].duration
+      let y=exercises[i].wait
+      let z=exercises[i].times
+
+      for(let j=0;j<z;j++)
+      {
+        playsound()
+        setTimeout(()=>{playsound()},x*1000)
+      }
+    }
+  }
+
   const appendid = (str, c)=>{
       str+=c
       return str
@@ -32,15 +60,11 @@ function App() {
     let waitid=appendid(activity.sno,'w')
     let timesid=appendid(activity.sno,'t')
     let pgid=appendid(activity.sno,'p')
-
-    console.log(waitid)
-    console.log(document.getElementById(waitid))
     
     activity.name=document.getElementById(activity.sno).value
     activity.duration=strToint(document.getElementById(durid).value)
     activity.wait=strToint(document.getElementById(waitid).value)
     activity.times=strToint(document.getElementById(timesid).value)
-    console.log(activity)
 
     let n=exercises.length
     for(let i=0;i<n-1;i++)
@@ -51,7 +75,6 @@ function App() {
         break
       }
     }
-    console.log(exercises)
 
     setExercises(exercises)
   }
@@ -76,7 +99,7 @@ function App() {
   return (
     <>
     <Header title="Exercise counter"/>
-    <Exercises exercises={exercises} onDelete={onDelete} onSet={onSet}/>
+    <Exercises exercises={exercises} onDelete={onDelete} onSet={onSet} onStart={onStart}/>
     </>
   );
 }
